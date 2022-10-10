2565e60b416f876abc25bd64842d6f2a6c945832
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-10-10 14:57:02 +0100

    Add functionality to links

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const navBar = document.getElementById('navbar');
 const navIcon = document.getElementById('nav-icon');
 const navMenuLinks = document.getElementById('nav-menu-links');
 const body = document.querySelector('body');
+const links = document.querySelectorAll('.nav_menu__container a');
+const main = document.querySelector('.headline');
+const projects = document.querySelector('.projects');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.footer__container');
 
 body.addEventListener('scroll', () => {
   if (navBar.scrollTop !== 0) navBar.style.boxShadow = '2px 2px 2px 1px rgba(0, 0, 0, 0.2);';
 });
 
-navIcon.addEventListener('click', () => {
+function handler(){
   if (navIcon.firstElementChild.id === 'hamburger-icon') {
     body.style.overflowX = 'hidden';
     navIcon.firstElementChild.src = './img/close-icon.svg';
@@ -20,4 +25,19 @@ navIcon.addEventListener('click', () => {
 
   navMenuLinks.classList.toggle('links_active');
   navBar.classList.toggle('nav_active');
-});
+  main.classList.toggle('backdrop_filter');
+  projects.classList.toggle('backdrop_filter');
+  about.classList.toggle('backdrop_filter');
+  contact.classList.toggle('backdrop_filter');
+}
+
+navIcon.addEventListener('click', handler);
+
+
+window.addEventListener('resize', () => {
+  if(window.innerWidth < 820){
+    links.forEach(link => {
+      link.addEventListener('click', handler);
+    });
+  }
+})
