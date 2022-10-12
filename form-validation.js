14ae37debc1d4831a616f7c0da4a56cf582d11f5
Kweeka <mtalbi29@yahoo.com>
2022-10-12 11:13:49 +0300

    Add form-validation.js, Added validation for email and username fields

---
+++ form-validation.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form');
+const emailField = document.getElementById('email')
+
+const showErrorMessage = () => {
+  
+}
+
+const validateEmail = (text) => {
+  const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  
+  if (emailPattern.test(text)) {
+    showErrorMessage("Email is invalid!");
+    return false;
+  }
+
+  if (/[A-Z]/.test(text)) {
+    showErrorMessage("Email must be in lowercase letters!");
+    return false;
+  }
+
+  return true;
+}
+
+const validateUsername = (text) => {
+  const username = text.trim()
+  const usernamePattern = /[0-9a-zA-Z\W]+/
+  
+  if (usernamePattern.test(username)) {
+    showErrorMessage("Please use only letters and numbers in username field!");
+    return false;
+  }
+
+  if (username.length > 30) {
+    showErrorMessage("Username must be less than 30 characters");
+    return false;
+  }
+
+  return true;
+}
+
+
+form.onsubmit = (ev) => {
+  ev.preventDefault();
+  validateEmail(emailField.value);
+}
