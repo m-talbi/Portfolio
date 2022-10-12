3bdeb273bdb4872fbd4c1935ea40b64bade948c5
Kweeka <mtalbi29@yahoo.com>
2022-10-12 13:16:05 +0300

    Update form-validation.js, Made errors shown under inputs, fixed some bugs

--- form-validation.js
+++ form-validation.js
@@ -1,60 +1,59 @@
 const form = document.getElementById('form');
 const emailField = document.getElementById('email');
-const error = document.querySelector('.form-error');
-const usernameField  = document.getElementById('name');
+const usernameField = document.getElementById('name');
 
-const showErrorMessage = (message) => {
-  const htmlEle = `
-    <p>${message}</p>
-  `;
-  error.style.display = 'block';
-  error.insertAdjacentHTML('beforeend', htmlEle);
-}
+const showErrorMessage = (message, containerEl) => {
+  const errorEl = `<p class="form-error">${message}</p>`;
+  containerEl.parentElement.insertAdjacentHTML('beforeend', errorEl);
+};
 
-const resetError = () => {
-  error.style.display = 'none';
-  error.textContent = '';
-}
+const hideErrorMessages = () => {
+  emailField.parentElement.querySelectorAll('.form-error')?.forEach((error) => error.remove());
+  usernameField.parentElement.querySelectorAll('.form-error')?.forEach((error) => error.remove());
+};
 
 const validateEmail = (text) => {
-  const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   if (!emailPattern.test(text)) {
-    showErrorMessage("Email is invalid!");
+    showErrorMessage('Email is invalid!', emailField);
     return false;
   }
 
   if (/[A-Z]/.test(text)) {
-    showErrorMessage("Email must be in lowercase letters!");
+    showErrorMessage('Email must be in lowercase letters!', emailField);
     return false;
   }
 
   return true;
-}
+};
 
 const validateUsername = (text) => {
-  const username = text.trim()
-  const usernamePattern = /[0-9a-zA-Z\W]+/
-  
-  if (!usernamePattern.test(username)) {
-    showErrorMessage("Please use only letters and numbers in username field!");
+  const username = text.trim();
+  const invalidCharactersPattern = /[\\/:"'*?<>@|&$,%!§;.]+/;
+
+  if (invalidCharactersPattern.test(username)) {
+    showErrorMessage('Please use only letters and numbers in username field!', usernameField);
     return false;
   }
 
   if (username.length > 30) {
-    showErrorMessage("Username must be less than 30 characters");
+    showErrorMessage('Username must be less than 30 characters', usernameField);
     return false;
   }
 
   return true;
-}
-
+};
 
 form.onsubmit = (ev) => {
-  //debugger
   ev.preventDefault();
-  validateUsername(usernameField.value);
-  validateEmail(emailField.value);
-}
 
-form.oninput = resetError;
+  hideErrorMessages();
+
+  const isUsernameValid = validateUsername(usernameField.value);
+  const isEmailValid = validateEmail(emailField.value);
+
+  return isUsernameValid && isEmailValid ? form.submit() : false;
+};
+
+form.oninput = hideErrorMessages;
