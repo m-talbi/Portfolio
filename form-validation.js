8328796095dd11a8e531db52a183e241e64da5c7
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-10-12 11:38:31 +0100

    Add showMessage and resetError functions

--- form-validation.js
+++ form-validation.js
@@ -1,14 +1,25 @@
 const form = document.getElementById('form');
-const emailField = document.getElementById('email')
+const emailField = document.getElementById('email');
+const error = document.querySelector('.form-error');
+const usernameField  = document.getElementById('name');
 
-const showErrorMessage = () => {
-  
+const showErrorMessage = (message) => {
+  const htmlEle = `
+    <p>${message}</p>
+  `;
+  error.style.display = 'block';
+  error.insertAdjacentHTML('beforeend', htmlEle);
+}
+
+const resetError = () => {
+  error.style.display = 'none';
+  error.textContent = '';
 }
 
 const validateEmail = (text) => {
   const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   
-  if (emailPattern.test(text)) {
+  if (!emailPattern.test(text)) {
     showErrorMessage("Email is invalid!");
     return false;
   }
@@ -25,7 +36,7 @@ const validateUsername = (text) => {
   const username = text.trim()
   const usernamePattern = /[0-9a-zA-Z\W]+/
   
-  if (usernamePattern.test(username)) {
+  if (!usernamePattern.test(username)) {
     showErrorMessage("Please use only letters and numbers in username field!");
     return false;
   }
@@ -40,6 +51,10 @@ const validateUsername = (text) => {
 
 
 form.onsubmit = (ev) => {
+  //debugger
   ev.preventDefault();
+  validateUsername(usernameField.value);
   validateEmail(emailField.value);
 }
+
+form.oninput = resetError;
