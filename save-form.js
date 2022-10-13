5672db17cc89429a6953f8c57bdaa325650d528f
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-10-13 10:00:59 +0100

    Add window listner to save-form.js

--- save-form.js
+++ save-form.js
@@ -3,6 +3,16 @@ const contactEmailField = document.getElementById('email');
 const contactUsernameField = document.getElementById('name');
 const contactMessageField = document.getElementById('textarea');
 
+window.onload = () => {
+  const isDataExist = JSON.parse(localStorage.getItem('contact-form'));
+  
+  if(!isDataExist) return;
+
+  contactUsernameField.value = isDataExist?.username;
+  contactEmailField.value = isDataExist?.email;
+  contactMessageField.value = isDataExist?.message;
+};
+
 contactForm.oninput = () => {
   const username = contactUsernameField.value;
   const email = contactEmailField.value;
